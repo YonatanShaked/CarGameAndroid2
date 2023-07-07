ceeb14cc5800276bb8da1dc20f40738e96f2e80a
Yonatan Shaked <theproguy12@gmail.com>
2023-07-08 00:20:33 +0300

    Update TopTen.java

--- app/src/main/java/com/example/homework1/models/TopTen.java
+++ app/src/main/java/com/example/homework1/models/TopTen.java
@@ -3,7 +3,6 @@ package com.example.homework1.models;
 import java.util.ArrayList;
 
 public class TopTen {
-    public final static int MAX_IN_LIST = 10;
     private final ArrayList<Record> records = new ArrayList<>();
 
     public ArrayList<Record> getRecords() {
