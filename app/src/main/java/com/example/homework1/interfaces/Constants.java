893d6e644fc23538a342aceff4fb4586d27d7c18
Yonatan Shaked <theproguy12@gmail.com>
2023-07-07 21:32:41 +0300

    Update Constants.java

--- app/src/main/java/com/example/homework1/interfaces/Constants.java
+++ app/src/main/java/com/example/homework1/interfaces/Constants.java
@@ -1,18 +1,9 @@
 package com.example.homework1.interfaces;
 
 public interface Constants {
-    int NUMBER_OF_HEARTS = 3;
-    int NUMBER_OF_ROADS = 5;
-    int FIRST_ROAD = 0;
-    int THIRD_ROAD = 2;
-    int FIFTH_ROAD = 4;
-    int ERROR = -1;
     int ANIM_DUR = 3000;
     int LOCATION_REQUEST_CODE = 10001;
     String SP_FILE = "MY_SP";
-    String EXTRA_KEY_DISTANCE = "EXTRA_KEY_DISTANCE";
-    String EXTRA_KEY_TOP = "EXTRA_KEY_TOP";
-    String EXTRA_KEY_SCORE = "EXTRA_KEY_SCORE";
     String KEY_TOP_TEN = "KEY_TOP_TEN";
 
 }
